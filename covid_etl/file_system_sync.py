0610bc971955e3ef4753c1cccf1287e57c5a98d6
hugo-espinelli <hugoespinelli@gmail.com>
2021-12-17 22:40:28 -0300

    add call rsync module

--- covid_etl/file_system_sync.py
+++ covid_etl/file_system_sync.py
@@ -1,4 +1,5 @@
 import logging
+import subprocess
 
 from watchdog.events import FileSystemEventHandler
 
@@ -8,10 +9,18 @@ logger = logging.getLogger()
 
 
 class FileSystemSync(FileSystemEventHandler):
-    def __init__(self, destination) -> None:
+    def __init__(self, destination, listen) -> None:
         super().__init__()
         self._destination = destination
+        self._listen_folder = listen
 
     def on_any_event(self, event):
         logger.info(f"Event detected {event.event_type }. Starting transfer process...")
-        put(event.src_path, self._destination)
+        # put(event.src_path, self._destination)
+        output = subprocess.run(
+            f"rsync -avz --rsh='ssh -p799' {self._listen_folder} alunos_uerj@157.86.153.23:{self._destination}",
+            shell=True,
+            capture_output=True    
+        )
+        logger.info(output)
+        
